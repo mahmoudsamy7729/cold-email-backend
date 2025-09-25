3fb5687b9dbb37edb29e262619dfbe5f20d827fb
mahmoudsamy7729 <mahmoud.samy7729 @gmail.com>
2025-09-25 11:07:14 +0300

    retrieve contacts count for each audience

--- audience/views.py
+++ audience/views.py
@@ -2,8 +2,10 @@ from django.db import IntegrityError
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
+from django.db.models import Q, Count
 
 
+from contacts.models import ContactStatus
 from .models import Audience
 from .serializers import AudienceSerializer
 
@@ -12,11 +14,12 @@ class AudienceViewSet(viewsets.ModelViewSet):
     serializer_class = AudienceSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ["name", "slug", "description"]
+    search_fields = ["name"]
     ordering_fields = ["updated_at", "created_at", "name"]
+    ordering = ["-created_at"] 
 
     def get_queryset(self):
-        return Audience.objects.filter(user=self.request.user)
+        return Audience.objects.filter(user=self.request.user).annotate(contacts_count=Count("contacts",~Q(contacts__status=ContactStatus.ARCHIVED)))
     
     def perform_destroy(self, instance):
         instance.archive()
